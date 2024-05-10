5ab1fc522d0d66b4e0fba9207566bc163d1f043f
fatmanuryikilmaz <125798606+fatmanuryikilmaz@users.noreply.github.com>
2024-05-10 16:45:38 +0300

    Initial commit

---
+++ FullCoffee.Core/ProductDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullCoffee.Core
+{
+    public class ProductDetail
+    {
+        public int Id { get; set; }
+        public int FavoryCount { get; set; }
+        public string Description { get; set; }
+        public int ReviewCount { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ FullCoffee.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullCoffee.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);//datayı aldıktan sonra belki sorgu isteyebilirim o yüzden IEnumarable kullanmadım
+        Task<T> GetByIdAsync(int id);   
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity); // efcore takip ettiği product ın sadece state ini değiştirir uzun sürmez o yüzden async yok 
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
